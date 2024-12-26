1d019d769c5e40265f3de8ef81915526db73ce36
Francesc Vila <francescvilasubias@gmail.com>
2024-12-26 11:08:24 +0100

    feat: add commands to set and remove breakpoints using regex

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,111 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "regex-breakpoints.setBreakpoints",
+      async (args?: any) => {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+          vscode.window.showErrorMessage("No active editor found!");
+          return;
+        }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "regex-breakpoints" is now active!');
+        const regexInput =
+          args?.providedRegex ??
+          (await vscode.window.showInputBox({
+            prompt: "Enter the regex pattern to match lines for breakpoints",
+            placeHolder: "e.g., console\\.log",
+          }));
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('regex-breakpoints.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Regex Breakpoints!');
-	});
+        if (!regexInput) {
+          vscode.window.showWarningMessage("Regex input is required.");
+          return;
+        }
 
-	context.subscriptions.push(disposable);
+        let regex: RegExp;
+        try {
+          regex = new RegExp(regexInput);
+        } catch (e) {
+          vscode.window.showErrorMessage("Invalid regex pattern!");
+          return e;
+        }
+
+        const document = editor.document;
+        const breakpoints: vscode.SourceBreakpoint[] = [];
+
+        for (let line = 0; line < document.lineCount; line++) {
+          const lineText = document.lineAt(line).text;
+          if (regex.test(lineText)) {
+            const uri = document.uri;
+            const position = new vscode.Position(line, 0);
+            const breakpoint = new vscode.SourceBreakpoint(
+              new vscode.Location(uri, position)
+            );
+            breakpoints.push(breakpoint);
+          }
+        }
+
+        vscode.debug.addBreakpoints(breakpoints);
+        vscode.window.showInformationMessage(
+          `${breakpoints.length} breakpoints added.`
+        );
+      }
+    )
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "regex-breakpoints.removeBreakpoints",
+      async (args?: any) => {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+          vscode.window.showErrorMessage("No active editor found!");
+          return;
+        }
+
+        const regexInput =
+          args?.providedRegex ??
+          (await vscode.window.showInputBox({
+            prompt:
+              "Enter the regex pattern to match lines for removing breakpoints",
+            placeHolder: "e.g., console\\.log",
+          }));
+
+        if (!regexInput) {
+          vscode.window.showWarningMessage("Regex input is required.");
+          return;
+        }
+
+        let regex: RegExp;
+        try {
+          regex = new RegExp(regexInput);
+        } catch (e) {
+          vscode.window.showErrorMessage("Invalid regex pattern!");
+          return;
+        }
+
+        const document = editor.document;
+
+        const existingBreakpoints = vscode.debug.breakpoints.filter((bp) => {
+          if (bp instanceof vscode.SourceBreakpoint) {
+            const line = bp.location.range.start.line;
+            const lineText = document.lineAt(line).text;
+            return (
+              bp.location.uri.toString() === document.uri.toString() &&
+              regex.test(lineText)
+            );
+          }
+          return false;
+        });
+
+        vscode.debug.removeBreakpoints(existingBreakpoints);
+        vscode.window.showInformationMessage(
+          `${existingBreakpoints.length} breakpoints removed.`
+        );
+      }
+    )
+  );
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
